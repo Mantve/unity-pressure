00135966e2b7371afad8e98b2cbde350bc575ae1
Mantvydas <mantvydasri99@gmail.com>
2020-07-15 22:55:19 +0300

    Moved project to 3d, added menu switch with Esc
    Manu switch code is quick but obsolete already, redo in the future

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject PortRenderer ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PortRenderer = GameObject.Find("PortRenderer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            PortRenderer.active = !PortRenderer.active;
+        }
+    }
+}
